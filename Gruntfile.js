95ff574a87c816937bee5eb227eefa1299f50501
corwin.tomkinson <corwin.tomkinson@axonn.co.uk>
2013-08-29 15:35:17 +0100

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        nospawn: true,
+      }
+    },
+	jasmine: {
+      base: {
+		  src: "build/js/vjsplugincomponents.js",
+		  options: {
+			specs: "build/test/vjsplugincomponentstests.js",
+			vendor: "components/jquery/jquery.min.js",
+		  }
+	  }
+    },
+	typescript: {
+      src: {
+        src: ['src/ts/*.ts'],
+        dest: 'build/js/vjsplugincomponents.js',
+        options: {
+          module: 'amd',
+          target: 'es3', 
+          base_path: 'src/ts',
+          sourcemap: true,
+          fullSourceMapPath: true,
+          declaration: true,
+        }
+      },
+	  test: {
+        src: ['test/*.ts'],
+        dest: 'build/test/vjsplugincomponentstests.js',
+        options: {
+          module: 'amd',
+          target: 'es3',
+          base_path: 'test',
+          sourcemap: true,
+          fullSourceMapPath: true,
+          declaration: true,
+        }
+      }
+    }
+  });
+  grunt.registerTask('test', ['typescript:test','jasmine']);   
+  grunt.registerTask('build', ['test', 'typescript:src']);
+};
